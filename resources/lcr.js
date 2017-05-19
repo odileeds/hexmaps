34e4fc785bd0ab65bbcd3329a70d61f88db9f402
Stuart <gravatar@strudel.org.uk>
2017-05-19 16:36:53 +0100

    Add Leeds City Region wards

---
+++ resources/lcr.js
@@ -0,0 +1,243 @@
+function Builder(id,w,h,padding,file){
+
+	this.w = w;
+	this.h = h;
+	this.aspectratio = w/h;
+	this.side = 16;
+	this.id = id;
+
+	this.hex = new HexMap({
+		'id':id,
+		'width':w,
+		'height':h,
+		'size':this.side,
+		'padding':padding,
+		'style': {'selected':{'fill-opacity':0.5 },'default':{'fill-opacity':0.5,'fill':'#cccccc'}}
+	});
+
+	this.hex.load(file,{me:this},function(e){ e.data.me.setColours("region"); });
+
+	this.hex.on('mouseover',{'builder':this},function(e){
+		e.data.builder.label(this.attr('title'));
+		this.attr({'fill-opacity':0.8});
+	}).on('mouseout',function(e){
+		S('.infobubble').remove();
+		this.attr({'fill-opacity':0.5});
+	}).on('click',{'builder':this},function(e){
+		e.data.hexmap.regionToggleSelected(e.data.region,true);
+		e.data.builder.label(this.attr('title'));
+	});
+	
+	
+	this.label = function(l){
+		if(S('.infobubble').length == 0) S('#'+this.id+'').after('<div class="infobubble"><div class="infobubble_inner"></div></div>');
+		S('.infobubble_inner').html(l);
+		return this;
+	}
+
+	this.saveable = (typeof Blob==="function");
+
+	// Update text of button
+	if(this.saveable){
+		// Add event to button
+		S('#save').on('click',{me:this},function(e){ e.data.me.save(); });
+		// Add key binding
+		S(document).on('keypress',{me:this},function(e){
+			if(e.originalEvent.charCode==109) S('#savesvg').trigger('click');	// M
+			if(e.originalEvent.charCode==104) S('#save').trigger('click');		// H
+		});
+
+		// Add event to button
+		S('#savesvg').on('click',{me:this},function(e){ e.data.me.saveSVG(); });
+
+	}else{
+		S('#save').css({'display':'none'});
+		S('#savesvg').css({'display':'none'});
+	}
+
+
+	function updateClass(btn){
+		S('.switchdata').addClass('b5-bg').removeClass('c10-bg');btn.removeClass('b5-bg').addClass('c10-bg');
+	}
+	
+
+	this.saveSVG = function(){
+
+		// Make hex json
+		var str = this.hex.paper.canvas.html();
+		this.save(str,"map.svg",'text/application/svg+xml');
+
+		return this;
+	}
+
+	this.save = function(str,file,type){
+
+		// Make hex json
+
+		if(!str) str = JSON.stringify(this.hex.mapping).replace(/\}\,/g,"},\n\t\t").replace(/\}\}\}/,"}\n\t\}\n\}").replace(/\"hexes\":{/,"\n\t\"hexes\": {\n\t\t").replace(/{"layout"/,"{\n\t\"layout\"");
+		if(!file) file = "test.hexjson";
+		if(!type) type = 'text/application/json';
+
+		var textFileAsBlob = new Blob([str], {type:type});
+		var fileNameToSaveAs = file;
+	
+		function destroyClickedElement(event){ document.body.removeChild(event.target); }
+		var dl = document.createElement("a");
+		dl.download = fileNameToSaveAs;
+		dl.innerHTML = "Download File";
+		if(window.webkitURL != null){
+			// Chrome allows the link to be clicked
+			// without actually adding it to the DOM.
+			dl.href = window.webkitURL.createObjectURL(textFileAsBlob);
+		}else{
+			// Firefox requires the link to be added to the DOM
+			// before it can be clicked.
+			dl.href = window.URL.createObjectURL(textFileAsBlob);
+			dl.onclick = destroyClickedElement;
+			dl.style.display = "none";
+			document.body.appendChild(dl);
+		}
+		dl.click();
+		return this;
+	}
+
+
+	function getColour(pc,a,b){
+		return 'rgb('+parseInt(a.rgb[0] + (b.rgb[0]-a.rgb[0])*pc)+','+parseInt(a.rgb[1] + (b.rgb[1]-a.rgb[1])*pc)+','+parseInt(a.rgb[2] + (b.rgb[2]-a.rgb[2])*pc)+')';
+	}
+
+	this.setColours = function(){
+
+		this.hex.setColours = function(region){
+			if(this.mapping.hexes[region].colour) return this.mapping.hexes[region].colour;
+			if(this.mapping.hexes[region].color) return this.mapping.hexes[region].color;
+			var rs = {'YH':'#F9BC26','EM':'#00B6FF','WM':'#E6007C','EA':'#FF6700','SC':'#2254F4','NI':'#722EA5','WA':'#0DBC37','NW':'#1DD3A7','NE':'#D60303','SW':'#178CFF','LO':'#D73058','SE':'#67E767'};
+			if(this.mapping.hexes[region].a && rs[this.mapping.hexes[region].a]) return rs[this.mapping.hexes[region].a];
+			return '#EF3AAB';
+		}
+		
+		this.hex.updateColours();
+
+		return this;
+	}
+	
+	// Function to parse a CSV file and return a JSON structure
+	// Guesses the format of each column based on the data in it.
+	function CSV2JSON(data,start,end){
+
+		// If we haven't sent a start row value we assume there is a header row
+		if(typeof start!=="number") start = 1;
+		// Split by the end of line characters
+		if(typeof data==="string") data = data.split(/[\n\r]+/);
+		// The last row to parse
+		if(typeof end!=="number") end = data.length;
+
+		if(end > data.length){
+			// Cut down to the maximum length
+			end = data.length;
+		}
+		
+		var line,datum,header,types;
+		var newdata = new Array();
+		var formats = new Array();
+		var req = new Array();
+
+		for(var i = 0, rows = 0 ; i < end; i++){
+
+			// If there is no content on this line we skip it
+			if(data[i] == "") continue;
+
+			// Split the line by commas (but not commas within quotation marks
+			line = data[i].split(/,(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))/);
+
+			datum = new Array(line.length);
+			types = new Array(line.length);
+
+			// Loop over each column in the line
+			for(var j=0; j < line.length; j++){
+
+				// Remove any quotes around the column value
+				datum[j] = (line[j][0]=='"' && line[j][line[j].length-1]=='"') ? line[j].substring(1,line[j].length-1) : line[j];
+
+				// If the value parses as a float
+				if(typeof parseFloat(datum[j])==="number" && parseFloat(datum[j]) == datum[j]){
+					types[j] = "float";
+					// Check if it is actually an integer
+					if(typeof parseInt(datum[j])==="number" && parseInt(datum[j])+"" == datum[j]){
+						types[j] = "integer";
+						// If it is an integer and in the range 1700-2100 we'll guess it is a year
+						if(datum[j] >= 1700 && datum[j] < 2100) types[j] = "year";
+					}
+				}else if(datum[j].search(/^(true|false)$/i) >= 0){
+					// The format is boolean
+					types[j] = "boolean";
+				}else if(datum[j].search(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&\/\/=]*)/) >= 0){
+					// The value looks like a URL
+					types[j] = "URL";
+				}else if(!isNaN(Date.parse(datum[j]))){
+					// The value parses as a date
+					types[j] = "datetime";
+				}else{
+					// Default to a string
+					types[j] = "string";
+					// If the string value looks like a time we set it as that
+					if(datum[j].search(/^[0-2]?[0-9]\:[0-5][0-9]$/) >= 0) types[j] = "time";
+				}
+			}
+
+			if(i == 0 && start > 0) header = datum;
+			if(i >= start){
+				newdata[rows] = datum;
+				formats[rows] = types;
+				rows++;
+			}
+		}
+		
+		// Now, for each column, we sum the different formats we've found
+		var format = new Array(header.length);
+		for(var j = 0; j < header.length; j++){
+			var count = {};
+			var empty = 0;
+			for(var i = 0; i < newdata.length; i++){
+				if(!newdata[i][j]) empty++;
+			}
+			for(var i = 0 ; i < formats.length; i++){
+				if(!count[formats[i][j]]) count[formats[i][j]] = 0;
+				count[formats[i][j]]++;
+			}
+			var mx = 0;
+			var best = "";
+			for(var k in count){
+				if(count[k] > mx){
+					mx = count[k];
+					best = k;
+				}
+			}
+			// Default
+			format[j] = "string";
+
+			// If more than 80% (arbitrary) of the values are a specific format we assume that
+			if(mx > 0.8*newdata.length) format[j] = best;
+
+			// If we have a few floats in with our integers, we change the format to float
+			if(format[j] == "integer" && count['float'] > 0.1*newdata.length) format[j] = "float";
+
+			req.push(empty == 0);
+
+		}
+
+		// Return the structured data
+		return { 'fields': {'name':header,'title':clone(header),'format':format,'required':req }, 'rows': newdata };
+	}
+
+
+	// Function to clone a hash otherwise we end up using the same one
+	function clone(hash) {
+		var json = JSON.stringify(hash);
+		var object = JSON.parse(json);
+		return object;
+	}
+
+	return this;
+
+}

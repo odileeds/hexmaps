cf45d618cc03c79d564d515cb63eb1f7fd7ebb79
Stuart <gravatar@strudel.org.uk>
2017-05-30 12:52:36 +0100

    Add colour scale keys and add gender split for GE2017 candidates

--- resources/constituencies.js
+++ resources/constituencies.js
@@ -21,6 +21,13 @@ function Constituencies(id,w,h,padding,file){
 			for(var i = 0; i < c.length; i++){
 				lbl += '<br /><strong><!--<a href="https://candidates.democracyclub.org.uk/person/'+c[i].i+'">-->'+c[i].n+'<!--</a>--></strong> - '+c[i].p;
 			}
+		}else if(e.data.builder.by == "gender"){
+			lbl = '<span style="border-bottom:1px solid #333;margin-bottom:0.25em;display:inline-block;">'+title+'</span>';
+			var c = e.data.hexmap.data['gender'][e.data.region];
+			lbl += '<br />Candidate gender split: '+c.ratio;
+			for(var i = 0; i < c.length; i++){
+				lbl += '<br /><strong>'+c[i].n+'</strong> - '+c[i].p+' ('+(c[i].g=="f" ? "Female" : (c[i].g=="m" ? "Male":"Unknown/diverse"))+')';
+			}
 		}else lbl = title+'<br />Region: '+rs[e.data.hexmap.mapping.hexes[e.data.region].a];
 		return lbl;
 	}	
@@ -77,6 +84,7 @@ function Constituencies(id,w,h,padding,file){
 	S('#colour-pty').on('click',{me:this},function(e){ e.data.me.setColours('party'); updateClass(this); });
 	S('#colour-ref').on('click',{me:this},function(e){ e.data.me.setColours('referendum'); updateClass(this); });
 	S('#colour-can').on('click',{me:this},function(e){ e.data.me.setColours('candidates'); updateClass(this); });
+	S('#colour-gen').on('click',{me:this},function(e){ e.data.me.setColours('gender'); updateClass(this); });
 
 	this.saveSVG = function(){
 
@@ -143,12 +151,15 @@ function Constituencies(id,w,h,padding,file){
 				'error':function(){},
 				'dataType':'csv'
 			});
-		}else if(type == "candidates"){
+		}else if(type == "candidates" || type == "gender"){
 			S().ajax('data/2017ge-candidates.json',{
 				'complete':function(d){
-					this.data[type] = d;
-					this.hex.data[type] = this.data[type];
+					this.data["candidates"] = d;
+					this.hex.data["candidates"] = this.data["candidates"];
 					this.setColours("candidates");
+					this.data["gender"] = d;
+					this.hex.data["gender"] = this.data["gender"];
+					this.setColours("gender");
 				},
 				'this': this,
 				'error':function(){},
@@ -179,6 +190,9 @@ function Constituencies(id,w,h,padding,file){
 	function getColour(pc,a,b){
 		return 'rgb('+parseInt(a.rgb[0] + (b.rgb[0]-a.rgb[0])*pc)+','+parseInt(a.rgb[1] + (b.rgb[1]-a.rgb[1])*pc)+','+parseInt(a.rgb[2] + (b.rgb[2]-a.rgb[2])*pc)+')';
 	}
+	function makeGradient(a,b){
+		return 'background: '+a.hex+'; background: -moz-linear-gradient(left, '+a.hex+' 0%, '+b.hex+' 100%);background: -webkit-linear-gradient(left, '+a.hex+' 0%,'+b.hex+' 100%);background: linear-gradient(to right, '+a.hex+' 0%,'+b.hex+' 100%);';
+	}
 
 	this.setColours = function(type){
 		if(!type) type = "region";
@@ -186,51 +200,83 @@ function Constituencies(id,w,h,padding,file){
 		if(type == "party" && (!this.data || !this.data["2015"])) return this.loadResults("2015");
 		if(type == "referendum" && (!this.data || !this.data["referendum"])) return this.loadResults("referendum");
 		if(type == "candidates" && (!this.data || !this.data["candidates"])) return this.loadResults("candidates");
+		if(type == "gender" && (!this.data || !this.data["gender"])) return this.loadResults("gender");
+
+		var key = "";
 
 		// Set the function for changing the colours
 		if(type == "population"){
+			var b = new Colour('#F9BC26');
+			var a = new Colour('#D60303');
+			var min = 50000;
+			var max = 80000;
 			this.hex.setColours = function(region){
-				var b = new Colour('#F9BC26');
-				var a = new Colour('#D60303');
-				var min = 50000;
-				var max = 80000;
 				var value = (this.mapping.hexes[region].p - min)/(max-min);
 				if(value < 0) value = 0;
 				if(value > 1) value = 1;
 				return getColour(value,a,b);
 			};
+			key = '&le;'+min+'<span style="'+makeGradient(a,b)+';width: 10em; height: 1em;opacity: 0.7;display: inline-block;margin: 0 0.25em;"></span>&ge;'+max;
 		}else if(type == "party"){
+			var names = {'Con':'Conservative','Lab':'Labour','LD':'Lib Dem','PC':'Plaid Cymru','Ind':'Independent','Spk':'Speaker'};
+			var p = {'Con':'#2254F4','Lab':'#D60303','LD':'#F9BC26','SNP':'#FF6700','PC':'#1DD3A7','UKIP':'#722EA5','Green':'#0DBC37','DUP':'#4f4c9a','SDLP':'#fbb675','SF':'#b6c727','UUP':'#EF3AAB','Ind':'#dfdfdf','Spk':'#909090'};
 			this.hex.setColours = function(region){
 				r = this.data["2015"][region];
-				var p = {'Con':'#2254F4','Lab':'#D60303','LD':'#F9BC26','SNP':'#FF6700','PC':'#1DD3A7','UKIP':'#722EA5','Green':'#0DBC37','DUP':'#4f4c9a','SDLP':'#fbb675','SF':'#b6c727','UUP':'#EF3AAB','Ind':'#dfdfdf','Spk':'#909090'};
 				return (p[r] || '#000');
 			}
+			for(var party in p){
+				key += '<span style="background-color:'+p[party]+';width: 1em; height: 1em;opacity: 0.7;display: inline-block;margin: 0 0.25em;"></span>'+(names[party] || party);
+			}
 		}else if(type == "referendum"){
+			var b = new Colour('#F9BC26');
+			var a = new Colour('#2254F4');
 			this.hex.setColours = function(region){
-				var b = new Colour('#F9BC26');
-				var a = new Colour('#2254F4');
 				return getColour(1 - (this.data["referendum"][region]-0.2)/0.6,a,b);
 			}
+			key = 'leave<span style="'+makeGradient(a,b)+';width: 10em; height: 1em;opacity: 0.7;display: inline-block;margin: 0 0.25em;"></span>remain';
 		}else if(type == "candidates"){
+			var levels = {0:'#2254F4',1:'#178CFF',2:'#00B6FF',3:'#08DEF9',4:'#1DD3A7',5:'#67E767',6:'#F9BC26'};
 			this.hex.setColours = function(region){
 				var n = this.data["candidates"][region].length;
 				var c = '#2254F4';
-				if(n > 1) c = '#178CFF';
-				if(n > 2) c = '#00B6FF';
-				if(n > 3) c = '#08DEF9';
-				if(n > 4) c = '#1DD3A7';
-				if(n > 5) c = '#67E767';
-				if(n > 6) c = '#F9BC26';
+				if(n > 0) c = (levels[n] || levels[6]);
 				return c;
 			}
+			key = '0';
+			for(var n in levels){
+				key += '<span style="background-color:'+levels[n]+';width: 1em; height: 1em;opacity: 0.7;display: inline-block;margin: 0 0.25em;"></span>';
+			}
+			key += '&ge;6';
+		}else if(type == "gender"){
+			var b = new Colour('#0DBC37');
+			var a = new Colour('#722EA5');
+			this.hex.setColours = function(region){
+				var m = 0;
+				var f = 0;
+				for(var i = 0; i < this.data["gender"][region].length; i++){
+					if(this.data["gender"][region][i].g=="f") f++;
+					if(this.data["gender"][region][i].g=="m") m++;
+				}
+				var t = m + f;
+				this.data["gender"][region].ratio = (t > 0 ? (m/(m+f)) : 0.5);
+				return getColour(this.data["gender"][region].ratio,a,b);
+			}
+			key = 'male<span style="'+makeGradient(b,a)+';width: 10em; height: 1em;opacity: 0.7;display: inline-block;margin: 0 0.25em;"></span>female';
+
 		}else if(type == "region"){
+			var names = {'YH':'Yorkshire and Humber','EM':'East Midlands','WM':'West Midlands','EA':'East Anglia','NI':'Northern Ireland','WA':'Wales','NW':'North West','NE':'North East','SE':'South East','SW':'South West','SC':'Scotland','LO':'London'};
+			var rs = {'YH':'#F9BC26','EM':'#00B6FF','WM':'#E6007C','EA':'#FF6700','SC':'#2254F4','NI':'#722EA5','WA':'#0DBC37','NW':'#1DD3A7','NE':'#D60303','SW':'#178CFF','LO':'#D73058','SE':'#67E767'};
 			this.hex.setColours = function(region){
 				var r = this.mapping.hexes[region].a;
-				var rs = {'YH':'#F9BC26','EM':'#00B6FF','WM':'#E6007C','EA':'#FF6700','SC':'#2254F4','NI':'#722EA5','WA':'#0DBC37','NW':'#1DD3A7','NE':'#D60303','SW':'#178CFF','LO':'#D73058','SE':'#67E767'};
 				return (rs[r] || this.colour.on);
+			}
+			for(var r in rs){
+				key += '<span style="background-color:'+rs[r]+';width: 1em; height: 1em;opacity: 0.7;display: inline-block;margin: 0 0.25em;"></span>'+(names[r] || r);
 			}
 		}
 		
+		S('#key').html((key ? key:''));
+
 		this.hex.updateColours();
 
 		return this;

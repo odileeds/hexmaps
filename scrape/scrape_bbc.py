e4d2d52846cc6dacbc8bf5e607c488b5132254cd
jz <jonathanzheng@hotmail.co.uk>
2019-12-13 21:11:38 +0000

    removed comment

--- scrape/scrape_bbc.py
+++ scrape/scrape_bbc.py
@@ -35,22 +35,3 @@ for code in codes:
 import json
 with open('results.json', 'w') as fp:
     json.dump(results, fp, indent=2)
-
-#s = open("E14000831.html").read()
-#tree = html.fromstring(s)
-#
-#title = tree.xpath('//h1[@class="constituency-title__title"]')[0].text
-#
-#rows = tree.xpath('//div[@class="ge2019-constituency-result__row"]')
-#votes = {}
-#for row in rows:
-#    party = row.xpath('./div/span[@class="ge2019-constituency-result__party-name"]')
-#    name = row.xpath('./div/span[@class="ge2019-constituency-result__candidate-name"]')
-#
-#    result = row.getnext()
-#    vote = result.xpath('./ul/li/span/span[@class="ge2019-constituency-result__details-value"]')
-#    votes[int(vote[0].text.replace(',',''))] = party[0].text
-#
-#mostvotes = max(votes.keys())
-#percent = float(mostvotes) / sum(votes.keys())
-#print("title: {} winner: {} votes: {} percent: {}".format(title, votes[mostvotes], mostvotes, int(percent*100)))

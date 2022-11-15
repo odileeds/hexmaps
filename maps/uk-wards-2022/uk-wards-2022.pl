a85a4adacf6d51acf2d5ffee5799c92faa11c9b7
slowe <299787+slowe@users.noreply.github.com>
2022-11-15 21:47:21 +0000

    Add wards by region

---
+++ maps/uk-wards-2022/uk-wards-2022.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/perl
+
+use utf8;
+use JSON::XS;
+use Data::Dumper;
+
+$hexjsonfile = "../uk-wards-2022.hexjson";
+
+if($ARGV[0] eq "split"){
+	splitRegions();
+}elsif($ARGV[0] eq "combine"){
+	combineRegions();
+}
+
+
+
+
+
+
+#############################
+
+sub getJSON {
+	my (@files,$str,@lines);
+	my $file = $_[0];
+	open(FILE,$file);
+	@lines = <FILE>;
+	close(FILE);
+	$str = join("",@lines);
+	return JSON::XS->new->decode($str);	
+}
+
+sub splitRegions {
+	print "Splitting regions in $hexjsonfile\n";
+	my ($regions,$id,$region,$fh);
+	my $hexjson = getJSON($hexjsonfile);
+	
+	foreach $id (keys(%{$hexjson->{'hexes'}})){
+#		print "$id\n";
+		$region = $hexjson->{'hexes'}{$id}{'region'};
+		if(!$regions->{$region}){
+			$regions->{$region} = {'layout'=>$hexjson->{'layout'},'hexes'=>{}};
+		}
+		$regions->{$region}{'hexes'}{$id} = $hexjson->{'hexes'}{$id};
+	}
+	
+	foreach $region (sort(keys(%{$regions}))){
+		print $region."\n";
+		open($fh,">",$region.".hexjson");
+		print $fh makeJSON($regions->{$region});
+		close($fh);
+	}
+}
+
+sub makeJSON {
+	my $json = shift;
+	
+	$txt = JSON::XS->new->utf8->canonical(1)->pretty->space_before(0)->encode($json);
+	
+	$txt =~ s/   /\t/g;
+
+	$txt =~ s/(\t{3}.*)\n/$1/g;
+	$txt =~ s/\,\t{3}/\, /g;
+	$txt =~ s/\t{2}\}(\,?)\n/ \}$1\n/g;
+	$txt =~ s/\{\n\t{3}/\{ /g;
+	
+	$txt =~ s/\"\: /\"\:/g;
+	$txt =~ s/\, \"/\,\"/g;
+	$txt =~ s/":\{ "/":\{"/g;
+	$txt =~ s/\" \},/\"\},/g;
+	
+	return $txt;
+}
+
+sub combineRegions {
+	opendir($dh,"./");
+	while( ($filename = readdir($dh))){
+		if($filename =~ /.hexjson$/){
+			print $filename."\n";
+		}
+	}
+	closedir($dh);
+}
